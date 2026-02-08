9c77273446ba462dbe519885a5e80287f4f6569d
aviraltrip <01fe24bcs086@kletech.ac.in>
2026-02-09 01:33:37 +0530

    added 11

---
+++ 800/11.cpp
@@ -0,0 +1,38 @@
+// © Aviral Tripathi.
+// For all your days prepare, And meet them ever alike:
+// When you are the anvil, bear — When you are the hammer, strike.
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define f(i, m, n) for(lli i = m; i < n; i++)
+#define fr(i, m, n) for(lli i = m; i >= n; i--)
+#define endl '\n'
+#define inf LLONG_MAX
+#define MOD 1e9+7 
+#define NFS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+void solutionForProblem() {
+    lli n;
+    cin >> n;
+    vector<lli> a(n);
+    f(i,0,n) cin >> a[i];
+    lli ans=LLONG_MAX;
+    f(i,0,n) {
+        if (a[i]==0) {
+            cout << 0 << endl;
+            return;
+        }
+        ans=min(ans,abs(a[i]));
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    NFS;
+    lli testCases = 1;
+    // cin >> testCases;
+    while (testCases--) {
+        solutionForProblem();
+    }
+    return 0;
+}
