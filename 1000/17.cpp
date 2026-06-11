7e99ec53544fb81573f04cf02800f8c72b2a07f9
aviraltrip <aviraltripathi25@gmail.com>
2026-06-11 18:21:16 +0530

    17 1000 added

---
+++ 1000/17.cpp
@@ -0,0 +1,37 @@
+// © Aviral Tripathi.
+// For all your ays prepare, And meet them ever alike:
+// When you are the anvil, bear — When you are the hammer, strike.
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define f(i, m, n) for(lli i = m; i < n; i++)
+#define fr(i, m, n) for(lli i = m; i >= n; i--)
+#define endl '\n'
+#define inf LLONG_MAX
+#define MOD 1e9+7 
+#define NFS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+ 
+void solutionForProblem() {
+    lli n;
+    cin >> n;
+    lli bound=1;
+    while(n--) {
+        cout << n << " ";
+        if (!(n&n-1)) {
+            bound=n; break;
+        }
+    }
+        lli i=0;
+        while (i<bound) {cout << i << " "; i++;}
+        cout << endl;
+}
+ 
+int main() {
+    NFS;
+    lli testCases = 1;
+    cin >> testCases;
+    while (testCases--) {
+        solutionForProblem();
+    }
+    return 0;
+}
