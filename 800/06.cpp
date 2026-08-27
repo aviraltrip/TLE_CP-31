0199597efb94484ea0d0b747cb49b33fd6f56233
aviraltrip <aviraltripathi25@gmail.com>
2026-08-28 00:23:10 +0530

    6 800 added

--- 800/06.cpp
+++ 800/06.cpp
@@ -12,7 +12,31 @@ using namespace std;
 #define NFS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
 void solutionForProblem() {
-    
+    lli n;
+    cin >> n;
+    vector<lli> a(n);
+    f(i,0,n) cin >> a[i];
+    if(n==2){
+        cout<<"Yes"<<endl;
+        return;
+    }
+    map<lli,lli> m;
+    f(i,0,n) m[a[i]]++;
+    bool flag=true;
+    if(m.size()>2) flag=false;
+    else if(m.size()==2) {
+        vector<lli> fre;
+        for(auto x:m) fre.push_back(x.second);
+        if(n%2==0) {
+            if(fre[0]!=n/2 || fre[1]!=n/2)
+                flag=false;
+        }
+        else {
+            if(abs(fre[0]-fre[1])>1)
+                flag=false;
+        }
+    }
+    (flag==true) ? cout<<"Yes"<<endl : cout<<"No"<<endl;
 }
 
 int main() {
