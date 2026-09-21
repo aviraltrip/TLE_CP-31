2934a86a445dcd2301f2f0ceff28992419d30170
aviraltrip <aviraltripathi25@gmail.com>
2026-09-21 16:12:16 +0530

    24 900 added

---
+++ 900/24.cpp
@@ -0,0 +1,34 @@
+// © Aviral Tripathi.
+// For all your ays prepare, And meet them ever alike:
+// When you are the anvil, bear — When you are the hammer, strike.
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define f(i, m, n) for(lli i = m; i < n; i++)
+#define fr(i, m, n) for(lli i = m; i >= n; i--)
+#define endl '\n'
+#define inf LLONG_MAX
+#define MOD 1e9+7 
+#define NFS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+void solutionForProblem() {
+    lli a,b,maxm=0,minm=0;
+    cin >> a >> b;
+    if (a==b) cout << 0 << " " << 0 << endl;
+    else {
+        lli d=abs(a-b);
+        maxm=d;
+        minm=min(a%d,d-a%d);
+        cout << maxm << " " << minm << endl;
+    }
+}
+
+int main() {
+    NFS;
+    lli testCases = 1;
+    cin >> testCases;
+    while (testCases--) {
+        solutionForProblem();
+    }
+    return 0;
+}
